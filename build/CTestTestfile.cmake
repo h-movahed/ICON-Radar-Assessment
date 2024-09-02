89a010887f05176952c199188f6598875301cf27
Hamovic <53612949+h-movahed@users.noreply.github.com>
2024-09-02 08:58:38 +0200

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/hmova/OneDrive/Dokumente/Dev/C_C++/Icon_Radar
+# Build directory: C:/Users/hmova/OneDrive/Dokumente/Dev/C_C++/Icon_Radar/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
